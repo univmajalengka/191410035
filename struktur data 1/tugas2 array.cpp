d9c089054ed1d939392c50b66f2bd223b12bde3c
deniahmad0908 <deniahmadgozali0908@gmail.com>
2020-03-06 10:20:33 +0700

    tambah file

---
+++ struktur data 1/tugas2 array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int nilai[6]{0,100, 90, 85, 80, 75};
+for(int i=0;i<6;i++)
+  cout<< " nilai ke["<<i<<"]="<<nilai[i];
+  cout<<endl;
+
+}
+
+
